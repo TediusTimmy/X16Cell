2a8861f3dc778a4cd3f1050de9f2407c2912a756
Thomas DiModica <ricinwich@yahoo.com>
2025-02-27 22:42:03 -0700

    Unit tests to cover the lines now uncovered.

--- tests/AddTest.c
+++ tests/AddTest.c
@@ -474,6 +474,15 @@ void testAdds (void)
 
    float_add(t, a, b); // 10000200 - 10000100 = 100
    ASSERT_EQ(c, t);
+
+   setFloat(a, 15, 0, 0x10, 0, 0, 0);
+   setFloat(b, 0, 0, 0x10, 0, 0, 0);
+   setFloat(c, 15, 0, 0x10, 0, 0, 0);
+
+   float_add(t, a, b); // 1000000000000000 + 1 = 1000000000000000
+   ASSERT_EQ(c, t);
+   float_add(t, b, a); // 1 + 1000000000000000 = 1000000000000000
+   ASSERT_EQ(c, t);
  }
 
 
